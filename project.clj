f4c17508a1ad52f1871388c900964d9ca3bd16de
Mikael Kopteff <miku.kopteff@gmail.com>
2013-02-25 20:43:02 +0200

    Compojure and lein structure

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject helsinki-info "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler helsinki-info.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
