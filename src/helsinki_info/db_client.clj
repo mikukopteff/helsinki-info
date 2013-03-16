5ac29c6e912270e9dc328d66e63dcc259a7300f2
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-16 15:25:19 +0200

    Database client can now batch insert test data

--- src/helsinki_info/db_client.clj
+++ src/helsinki_info/db_client.clj
@@ -2,25 +2,29 @@
   (:require [monger.core :as mongo])
   (:use [monger.collection :only [insert insert-batch]])
   (:import [com.mongodb MongoOptions ServerAddress]
-           [org.bson.types ObjectId]
            [com.mongodb DB WriteConcern]))
 
 
-(defn connect []
+(defn- connect []
   (let [^MongoOptions opts (mongo/mongo-options :threads-allowed-to-block-for-connection-multiplier 300)
       ^ServerAddress sa  (mongo/server-address "127.0.0.1" 27017)]
   (mongo/connect! sa opts)
   (mongo/set-db! (mongo/get-db "open-helsinki"))))
 
-(defn disconnect []
+(defn- disconnect []
   mongo/disconnect!)
 
-(defn withConnection [fun]
+(defn- withConnection [fun]
   (connect)
   fun
   (disconnect))
 
+(def- para 
+  "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.")
+
 (defn test-data []
   "This function is here until we have a place to get the actual http data"
   (withConnection 
-    (monger.collection/insert "events" { :_id (ObjectId.) :first_name "pyh" :last_name "poh" })))
+    (monger.collection/insert-batch "events" [{:heading "Decision made - maybe!" :paragraph para }
+                                              {:heading "Another decision!" :paragraph para }
+                                              {:heading "Decision decisions" :paragraph para }])))
