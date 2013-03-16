a3a5de92e6d010e3102fd6b475bb99ef82c77096
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-16 10:09:52 +0200

    Add bootstrap cos i'll be lazy

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,7 +1,7 @@
-require(['jquery', 'transparency'], function($, Transparency) {
+require(['jquery', 'transparency', 'bootstrap.min'], function($, Transparency) {
   var data = {
-    greeting: 'My',
-    name:     'skeleton'
+    greeting: 'Decision made - maybe!',
+    name:     'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
   };
 
   jQuery.fn.render = Transparency.jQueryPlugin;
