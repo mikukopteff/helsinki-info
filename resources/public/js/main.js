4aab08fe083f4531cba2e2b5742b243aa87f677d
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-16 15:03:19 +0200

    Add proper templating and fix minor html issue.

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,10 +1,19 @@
 require(['jquery', 'transparency', 'bootstrap.min'], function($, Transparency) {
-  var data = {
+  var events = [{
     heading: 'Decision made - maybe!',
     paragraph:     'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
-  };
+  }, 
+  {
+    heading: 'Another decision!',
+    paragraph:     'Boring stuff mainly here: Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
+  },
+  {
+    heading: 'Small decision here',
+    paragraph:     'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
+  }
+  ];
 
   jQuery.fn.render = Transparency.jQueryPlugin;
-  $('#template').render(data);
+  $('#events').render(events);
 
 });
