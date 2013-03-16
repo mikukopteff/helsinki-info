13bfed7288684d3adf347736af4b2b6fa8d2c6fa
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-16 14:32:51 +0200

    Change proper data bind attributes to transparency templates

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,7 +1,7 @@
 require(['jquery', 'transparency', 'bootstrap.min'], function($, Transparency) {
   var data = {
-    greeting: 'Decision made - maybe!',
-    name:     'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
+    heading: 'Decision made - maybe!',
+    paragraph:     'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
   };
 
   jQuery.fn.render = Transparency.jQueryPlugin;
