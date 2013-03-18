a2c354f40eab62535b7a7666de96b2106398b5c1
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-18 22:26:23 +0200

    Ajax query for fetching events data from server The very simple end-to-end functionaluty is now ready

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,19 +1,9 @@
 require(['jquery', 'transparency', 'bootstrap.min'], function($, Transparency) {
-  var events = [{
-    heading: 'Decision made - maybe!',
-    paragraph:     'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
-  }, 
-  {
-    heading: 'Another decision!',
-    paragraph:     'Boring stuff mainly here: Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
-  },
-  {
-    heading: 'Small decision here',
-    paragraph:     'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
-  }
-  ];
-
   jQuery.fn.render = Transparency.jQueryPlugin;
-  $('#events').render(events);
+  $.ajax('http://localhost:3000/events').done(
+    function(json){
+      $('#events').render(json);
+    });
+  
 
 });
