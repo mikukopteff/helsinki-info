f10d4c30b77d7123533e16d22f0f9a9949722b71
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-18 22:17:55 +0200

    Add correct content type to json reply

--- src/helsinki_info/handler.clj
+++ src/helsinki_info/handler.clj
@@ -9,7 +9,10 @@
 (defroutes app-routes
   (GET "/" [] (resp/file-response "index.html" {:root "resources/public"}))
   (GET "/ping" [] "pong")
-  (GET "/events" [] (json/write-str (db/find-events)))
+  (GET "/events" []
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body (json/write-str (db/find-events))})
   (route/resources "/")
   (route/not-found "Not Found"))
 
