9a13d7ebb0090e3ece05bd0b24b6164098a75fe3
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-19 19:30:04 +0200

    Refactor require clauses and add remove function

--- src/helsinki_info/db_client.clj
+++ src/helsinki_info/db_client.clj
@@ -1,6 +1,6 @@
 (ns helsinki-info.db-client
   (:require [monger.core :as mongo])
-  (:use [monger.collection :only [insert insert-batch]])
+  (:require [monger.collection :as mongo-collection])
   (:import [com.mongodb MongoOptions ServerAddress]
            [com.mongodb DB WriteConcern]))
 
@@ -22,12 +22,16 @@
     (disconnect)
     result))
 
-(defn events []
+(defn- events []
   (in-connection
-    (fn [] (doall (monger.collection/find-maps "events")))))
+    (fn [] (doall (mongo-collection/find-maps "events")))))
 
 (defn find-events []
   (map #(assoc % :_id (.toString (get % :_id))) (events)))
+
+(defn delete-events []
+  (in-connection
+    #(mongo-collection/remove "events")))
 
 (def para 
   "Kaupunginvaltuusto päättänee myöntää Helsinki Stadion Oy:lle 240 000 euron suuruisen vakuudettoman lainan urheilu- ja ulkoilulaitosrahaston varoista Sonera Stadiumin katsomon lasittamisen ja kattamisen rahoittamiseen seuraavin ehdoin:
@@ -38,6 +42,6 @@ Lainan erityisehto: Lainansaaja sitoutuu maksamaan lainasta pois sen osuuden, jo
 (defn insert-test-data []
   "This function is here until we have a place to get the actual http data"
   (in-connection 
-    (fn [] (monger.collection/insert-batch "events" [{:heading "§ 5 - Lainan myöntäminen Helsinki Stadion Oy:lle" :paragraph para}
+    (fn [] (mongo-collection/insert-batch "events" [{:heading "§ 5 - Lainan myöntäminen Helsinki Stadion Oy:lle" :paragraph para}
                                               {:heading "Ullanlinnan tontin 7/133/5 ja puistoalueen asemakaavan muuttaminen (nro 12117, Tehtaankatu 1d, Vuorimiehenkatu 8b)" :paragraph (str para para) }
                                               {:heading "Vartiokylän tontin 45475/1 asemakaavan muuttaminen (nro 12139; Vartioharjun hoivakoti)" :paragraph para }]))))
