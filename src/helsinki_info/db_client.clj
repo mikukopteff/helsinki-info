7b03cbca2f19314c258b88d2dd97fc337b130831
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-19 19:47:28 +0200

    Refactor test data adding to startup

--- src/helsinki_info/db_client.clj
+++ src/helsinki_info/db_client.clj
@@ -33,15 +33,6 @@
   (in-connection
     #(mongo-collection/remove "events")))
 
-(def para 
-  "Kaupunginvaltuusto päättänee myöntää Helsinki Stadion Oy:lle 240 000 euron suuruisen vakuudettoman lainan urheilu- ja ulkoilulaitosrahaston varoista Sonera Stadiumin katsomon lasittamisen ja kattamisen rahoittamiseen seuraavin ehdoin:
-Laina-aika: Laina on maksettava takaisin tasalyhennyksin kymmenen vuoden kuluessa kuitenkin siten, että ensimmäinen vuosi on lyhennyksistä vapaa.
-Lainan korko: Lainasta maksettava korko on peruskoron suuruinen.
-Lainan erityisehto: Lainansaaja sitoutuu maksamaan lainasta pois sen osuuden, joka ylittää 40 % lopullisista liikuntaviraston vahvistamista kokonaiskustannuksista.")
-
-(defn insert-test-data []
-  "This function is here until we have a place to get the actual http data"
+(defn insert-events [data]
   (in-connection 
-    (fn [] (mongo-collection/insert-batch "events" [{:heading "§ 5 - Lainan myöntäminen Helsinki Stadion Oy:lle" :paragraph para}
-                                              {:heading "Ullanlinnan tontin 7/133/5 ja puistoalueen asemakaavan muuttaminen (nro 12117, Tehtaankatu 1d, Vuorimiehenkatu 8b)" :paragraph (str para para) }
-                                              {:heading "Vartiokylän tontin 45475/1 asemakaavan muuttaminen (nro 12139; Vartioharjun hoivakoti)" :paragraph para }]))))
+    (fn [] (mongo-collection/insert-batch "events" data))))
--- src/helsinki_info/tasks.clj
+++ src/helsinki_info/tasks.clj
@@ -1,9 +1,25 @@
 (ns helsinki-info.tasks
-  (:use overtone.at-at))
-
-(defn startup[] 
-  (println "Starting server"))
+  (:use overtone.at-at)
+  (:require [helsinki-info.db-client :as db]))
 
 (def pool (mk-pool))
 
-(every 1000 #(println "Running task") pool)
+(defn start-scarping[]
+  (every 1000 #(println "Running task") pool))
+
+(def para 
+  "Kaupunginvaltuusto päättänee myöntää Helsinki Stadion Oy:lle 240 000 euron suuruisen vakuudettoman lainan urheilu- ja ulkoilulaitosrahaston varoista Sonera Stadiumin katsomon lasittamisen ja kattamisen rahoittamiseen seuraavin ehdoin:
+Laina-aika: Laina on maksettava takaisin tasalyhennyksin kymmenen vuoden kuluessa kuitenkin siten, että ensimmäinen vuosi on lyhennyksistä vapaa.
+Lainan korko: Lainasta maksettava korko on peruskoron suuruinen.
+Lainan erityisehto: Lainansaaja sitoutuu maksamaan lainasta pois sen osuuden, joka ylittää 40 % lopullisista liikuntaviraston vahvistamista kokonaiskustannuksista.")
+
+(defn- init-events []
+  "This function is here until we have a place to get the actual http data"
+  (db/delete-events)
+  (db/insert-events [{:heading "§ 5 - Lainan myöntäminen Helsinki Stadion Oy:lle" :paragraph para}
+                                              {:heading "Ullanlinnan tontin 7/133/5 ja puistoalueen asemakaavan muuttaminen (nro 12117, Tehtaankatu 1d, Vuorimiehenkatu 8b)" :paragraph (str para para) }
+                                              {:heading "Vartiokylän tontin 45475/1 asemakaavan muuttaminen (nro 12139; Vartioharjun hoivakoti)" :paragraph para }]))
+
+(defn startup[] 
+  (println "Starting server")
+  (init-events))
