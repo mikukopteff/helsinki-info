dd34ba07a0d255f9972aded2ba8d44bd00fce1f0
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-19 17:41:37 +0200

    Add id passing in UI between screens.

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,7 +1,19 @@
 require(['jquery', 'transparency', 'bootstrap.min'], function($, Transparency) {
   jQuery.fn.render = Transparency.jQueryPlugin;
-  $.ajax('http://localhost:3000/events').done(
-    function(json){
-      $('#events').render(json);
-    });
+  
+directives = {
+  oid: {
+    oid: function(params) {
+      return this._id;
+    },
+    href: function(params) {
+      return 'decision.html?id=' + this._id;
+    }
+  }
+};
+
+$.ajax('http://localhost:3000/events').done(
+  function(json){
+    $('#events').render(json, directives);
+  });
 });
