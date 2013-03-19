008d7aaec007f7b7628779ad6399c16ff35d8f56
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-19 16:22:36 +0200

    Change to better testa data

--- src/helsinki_info/db_client.clj
+++ src/helsinki_info/db_client.clj
@@ -30,11 +30,14 @@
   (map #(dissoc % :_id) (events)))
 
 (def para 
-  "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.")
+  "Kaupunginvaltuusto päättänee myöntää Helsinki Stadion Oy:lle 240 000 euron suuruisen vakuudettoman lainan urheilu- ja ulkoilulaitosrahaston varoista Sonera Stadiumin katsomon lasittamisen ja kattamisen rahoittamiseen seuraavin ehdoin:
+Laina-aika: Laina on maksettava takaisin tasalyhennyksin kymmenen vuoden kuluessa kuitenkin siten, että ensimmäinen vuosi on lyhennyksistä vapaa.
+Lainan korko: Lainasta maksettava korko on peruskoron suuruinen.
+Lainan erityisehto: Lainansaaja sitoutuu maksamaan lainasta pois sen osuuden, joka ylittää 40 % lopullisista liikuntaviraston vahvistamista kokonaiskustannuksista.")
 
 (defn insert-test-data []
   "This function is here until we have a place to get the actual http data"
   (in-connection 
-    (fn [] (monger.collection/insert-batch "events" [{:heading "Decision made - maybe!" :paragraph para }
-                                              {:heading "Another decision!" :paragraph (str para para) }
-                                              {:heading "Decision decisions" :paragraph para }]))))
+    (fn [] (monger.collection/insert-batch "events" [{:heading "§ 5 - Lainan myöntäminen Helsinki Stadion Oy:lle" :paragraph para}
+                                              {:heading "Ullanlinnan tontin 7/133/5 ja puistoalueen asemakaavan muuttaminen (nro 12117, Tehtaankatu 1d, Vuorimiehenkatu 8b)" :paragraph (str para para) }
+                                              {:heading "Vartiokylän tontin 45475/1 asemakaavan muuttaminen (nro 12139; Vartioharjun hoivakoti)" :paragraph para }]))))
