732853dad1c2ff08a81cff7f06c8064d1dcf050c
Mikael Kopteff <miku.kopteff@gmail.com>
2013-03-29 20:40:11 +0200

    Case data is now dynamic Id is parsed from query string and queried from backend

--- resources/public/js/case.js
+++ resources/public/js/case.js
@@ -1,7 +1,19 @@
 require(['jquery', 'transparency', 'underscore-min','bootstrap.min'], function($, Transparency) {
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.search);
+    if(results == null)
+      return "";
+    else
+      return decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
   jQuery.fn.render = Transparency.jQueryPlugin;
-  $.ajax('/events').done(
+  $.ajax('/event/' + getParameterByName("id")).done(
     function(json){
-      $('#decision').render(_.first(json));
+      $('#decision').render(json);
     });
+
 });
