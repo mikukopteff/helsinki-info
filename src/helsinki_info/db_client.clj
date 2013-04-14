1529c45f408c5f4b5948c493587ef050478d27ca
Mikael Kopteff <miku.kopteff@gmail.com>
2013-04-14 13:52:30 +0300

    Remove useless func doc

--- src/helsinki_info/db_client.clj
+++ src/helsinki_info/db_client.clj
@@ -53,7 +53,6 @@
     #(mongo-collection/remove "events")))
 
 (defn insert-events [data]
-  "This function need to check for oid and then added if it's not there! Check monger _id documentation"
   (in-connection 
     #(mongo-collection/insert-batch "events" (add-id data))))
 
