4c22191af65e6eda4c9dfa9f0dd8ff8466eec020
Mikael Kopteff <miku.kopteff@gmail.com>
2013-05-14 17:15:09 +0300

    Openahjo client can now fetch new data from api

--- src/helsinki_info/openahjo_client.clj
+++ src/helsinki_info/openahjo_client.clj
@@ -9,7 +9,7 @@
 
 (def base-url "http://dev.hel.fi")
 
-(def starting-url "/openahjo/v1/agenda_item/?format=json&offset=805&limit=5")
+(def starting-url "/openahjo/v1/agenda_item/?format=json&offset=0&limit=20")
 
 (def new-items-url "/openahjo/v1/agenda_item/?format=json&order_by=-last_modified_time")
 
@@ -18,6 +18,7 @@
   (json/read-str (:body (client/get (str base-url url)))))
 
 (defn- select-case [item]
+  "Fetches the db case if it is already stored. Case equals issue in the openahjo api"
   (let [case (get item "issue")]
     (let [existing-case (db/find-by-case (get case "slug"))]
       (if (empty? existing-case)
@@ -25,26 +26,44 @@
         existing-case))))
 
 (defn- update-existing [case]
+  (println "---------------------- updating")
   (info "updating an existing case")
-  (db/update case "cases"))
+  (db/update case "cases") 
+  true)
+
+(defn- update [old-case, new-item, old-items] 
+  (if-not (some #(=  (get % :id) (get new-item "id") ) old-items)
+    (update-existing (assoc old-case :items (conj old-items (dissoc new-item "issue")))) 
+    false))
 
 (defn- insert-new [case]
   (info "inserting new case")
-  (db/insert-single case "cases"))            
+  (db/insert-single case "cases") 
+  true)
 
-(defn- rearrange [item]
- (let [case (select-case item)]
+(defn- sort-items-and-store [item]
+  (let [case (select-case item)]
     (let [items (get case :items)]
       (if (nil? items)
         (insert-new (conj case {:items [(dissoc item "issue")]}))
-        (update-existing (assoc case :items (conj items (dissoc item "issue"))))))))
+        (update case item items)))))
+  
 
-(defn fetch-all-items 
-  ([] (fetch-all-items (call-openahjo starting-url)))
+(defn fetch-items 
+  ([] (fetch-all-items (call-openahjo new-items-url)))
   ([data]
-  "This function is mainly used to get all the data from the api"
-  (doall (map rearrange (get data "objects")))
-  (let [next-url (get (get data "meta") "next")]
-    (if-not (nil? next-url)
-      (fetch-all-items (call-openahjo next-url))
-      (info "Last item hit. Stopping items fetching")))))
+  (let [added-to-db (doall (take-while true? (map sort-items-and-store (get data "objects"))))]  
+    (if (= (count added-to-db) (count (get data "objects")))
+      (println "going for next page")
+      (let [next-url (get (get data "meta") "next")]
+        (if-not (nil? next-url)
+          (fetch-all-items (call-openahjo next-url))
+          (info "Last item hit. Stopping items fetching")))))))
+
+(defn fetch-new-items []
+  (fetch-items (call-openahjo new-items-url)))
+
+(defn fetch-all-items []
+  (fetch-items (call-openahjo starting-url)))
+
+
