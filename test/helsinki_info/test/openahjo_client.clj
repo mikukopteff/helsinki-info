e796df28bb92d1338ceffc7090ae85fbced25291
Mikael Kopteff <miku.kopteff@gmail.com>
2013-05-28 09:14:30 +0300

    Add line breaks between test

--- test/helsinki_info/test/openahjo_client.clj
+++ test/helsinki_info/test/openahjo_client.clj
@@ -19,11 +19,13 @@
   (testing "data is crawled and formatted to open helsinki format"
     (insert-test-data)
     (is (= 3 (count (find-collections "cases")))))
+  
   (testing "fetching will stop when encountering a stored item")
     (insert-test-data)
     (is (= 3 (count (find-collections "cases"))))
     (insert-test-data "test-resources/openahjo-next-5.json")
     (is (= 4 (count (find-collections "cases"))))
+  
   (testing "items are added correctly in cases"
     (let [case (find-by-case "hel-2012-013814")]
       (is (= 3 (count (get case :items))))
