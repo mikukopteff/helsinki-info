f59cf95ae3be63e78d156c20a01aa8a1427d7956
Mikael Kopteff <miku.kopteff@gmail.com>
2013-06-09 10:46:29 +0300

    Data in index page isnot copy paste anymore

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -42,7 +42,6 @@ require(['jquery', 'moment', 'utils', 'transparency', 'bootstrap'], function($, 
   function selectDataToShow(){
     var searchString = window.location.hash.replace('#q=', '');
     if (searchString === '') {
-      $('.row.listing').hide();
       fetchNewItems();
     } else {
       $('#search-input').val(searchString);
@@ -52,21 +51,25 @@ require(['jquery', 'moment', 'utils', 'transparency', 'bootstrap'], function($, 
 
   function fetchNewItems() {
     $.ajax('/item/newest').done(
-      function(json){
-        $('#new-first-row').render(json.splice(0, 2), directives);
-        $('#new-second-row').render(json.splice(0, 2), directives);
+      function(json) {
+        $('#listing .row').render(json.splice(0, 2), directives);
+        while (json.length > 0) {
+          elementRow.clone().appendTo('#listing').render(json.splice(0, 2), directives);
+        }
+
     });
   }
 
   function showSearchListing(json) {
-    $('.row.listing').fadeIn(500).render(json, directives);
+    var searchResults = elementRow.clone();
+    searchResults.children().first().removeClass('span6').addClass('span12');
+    searchResults.appendTo('#listing').fadeIn(500).render(json, directives);
     $('#listing-header').fadeIn(500).text('Haulla \'' + $('#search-input').val() + '\' löytyi ' + json.length + ' tulosta:')
   }
 
   function search(input) {
     if (input != 'undefined' && input != ''){
-      $('#listing-loading').show();
-      $('#listing').fadeOut(800);
+      $('#listing').children().fadeOut(800, function(){ this.remove() });
       $.ajax('/search/' + encodeURI(input)).done(showSearchListing);
     }
   }
@@ -77,8 +80,9 @@ require(['jquery', 'moment', 'utils', 'transparency', 'bootstrap'], function($, 
     search(input);
   }
 
+  Utils.ajaxLoader('#loading');
+  var elementRow = $('#listing .row').clone();
   selectDataToShow()
   $('#search').click(onSearchClicked);
-  Utils.ajaxLoader('#loading');
-
+  
 });
