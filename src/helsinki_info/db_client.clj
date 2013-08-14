3a145bef05c73a8d0ae50fa1097f970d44bbf45c
Juha Reunanen <juha.reunanen@gmail.com>
2013-08-14 11:03:40 +0300

    Revert "don't disconnect and connect to the db all the time"
    This reverts commit 768ff9dd0b03d023858d10258693db8576d9481b.

--- src/helsinki_info/db_client.clj
+++ src/helsinki_info/db_client.clj
@@ -16,13 +16,21 @@
   (:import [com.mongodb DB WriteConcern]
            [org.bson.types ObjectId]))
 
-(defn connect []
+(defn- connect []
   (debug "connecting to mongo")
   (mongo/connect-via-uri! ((utils/props):mongo.uri)))
 
-(defn disconnect []
+(defn- disconnect []
   (debug "disconnecting from mongo")
   (mongo/disconnect!))
+
+(defn- in-connection [fun]
+  (try
+    (connect)
+    (fun)
+    (catch Exception e
+      (debug (str "Catch exception in in-connection: " e)))
+    (finally (disconnect))))
 
 (def search-result-fields [:slug :summary :heading :items :meeting :subject])
 
@@ -39,35 +47,44 @@
   (map add-id coll))
 
 (defn find-collections [collection]
-  (#(map convert-id (doall (mongo-collection/find-maps collection)))))
+  (in-connection
+    #(map convert-id (doall (mongo-collection/find-maps collection)))))
 
 (defn find-by-case [id]
-  (#(convert-id (mongo-collection/find-one-as-map "cases" {:slug id}))))
+  (in-connection
+    #(convert-id (mongo-collection/find-one-as-map "cases" {:slug id}))))
 
 (defn delete [collection]
-  (#(mongo-collection/remove collection)))
+  (in-connection
+    #(mongo-collection/remove collection)))
 
 (defn insert [data, collection]
-  (#(mongo-collection/insert-batch collection (add-ids data))))
+  (in-connection
+    #(mongo-collection/insert-batch collection (add-ids data))))
 
 (defn update [data, collection]
-  (#(mongo-collection/update-by-id collection (ObjectId. (get data :_id)) (dissoc data :_id))))
+  (in-connection
+      #(mongo-collection/update-by-id collection (ObjectId. (get data :_id)) (dissoc data :_id))))
 
 (defn insert-single [element, collection]
-  (#(mongo-collection/insert collection (merge element {:_id (ObjectId.)}))))
+  (in-connection
+      #(mongo-collection/insert collection (merge element {:_id (ObjectId.)}))))
 
 (defn find-popular-new []
-  (#(mongo-collection/aggregate "cases" [{$unwind "$items"}, {$group {:_id "$slug" :sum_items {$sum 1}}}, {$match {:sum_items {$gte 3}}}])))
+  (in-connection
+    #(mongo-collection/aggregate "cases" [{$unwind "$items"}, {$group {:_id "$slug" :sum_items {$sum 1}}}, {$match {:sum_items {$gte 3}}}])))
 
 (defn find-newest-headings [page, per-page]
-  (#(doall (query/with-collection "cases"
+  (in-connection
+    #(doall (query/with-collection "cases"
       (query/find {})
       (query/fields search-result-fields)
       (query/sort (sorted-map "items.meeting.date" -1))
       (query/paginate :page page :per-page per-page)))))
 
 (defn- search-cases [search-query projection page per-page]
-  (#(doall (query/with-collection "cases"
+  (in-connection
+    #(doall (query/with-collection "cases"
               (query/find search-query)
               (query/fields projection)
               (query/paginate :page page :per-page per-page)))))
@@ -85,10 +102,12 @@
     (search-cases query projection page per-page)))
 
 (defn search [string]
-  (#(map (fn [result] (get result :obj))(search/results-from (search/search "cases" (search-utils/escape-search-string string))))))
+  (in-connection
+    #(map (fn [result] (get result :obj))(search/results-from (search/search "cases" (search-utils/escape-search-string string))))))
 
 (defn count-cases [search-query]
-  (#(mongo-collection/count "cases" search-query)))
+  (in-connection
+    #(mongo-collection/count "cases" search-query)))
   
 (defn count-cases-by-committee [committee_name]
   (count-cases {:items.meeting.committee_name committee_name}))
