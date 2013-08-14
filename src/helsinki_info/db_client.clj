749c354dea9ba5be0f85e9ab48f4db2c4c55a1ec
Juha Reunanen <juha.reunanen@gmail.com>
2013-08-14 09:53:52 +0300

    cleaned up syntax

--- src/helsinki_info/db_client.clj
+++ src/helsinki_info/db_client.clj
@@ -39,38 +39,38 @@
   (map add-id coll))
 
 (defn find-collections [collection]
-  (#(map convert-id (doall (mongo-collection/find-maps collection)))))
+  (map convert-id (doall (mongo-collection/find-maps collection))))
 
 (defn find-by-case [id]
-  (#(convert-id (mongo-collection/find-one-as-map "cases" {:slug id}))))
+  (convert-id (mongo-collection/find-one-as-map "cases" {:slug id})))
 
 (defn delete [collection]
-  (#(mongo-collection/remove collection)))
+  (mongo-collection/remove collection))
 
 (defn insert [data, collection]
-  (#(mongo-collection/insert-batch collection (add-ids data))))
+  (mongo-collection/insert-batch collection (add-ids data)))
 
 (defn update [data, collection]
-  (#(mongo-collection/update-by-id collection (ObjectId. (get data :_id)) (dissoc data :_id))))
+  (mongo-collection/update-by-id collection (ObjectId. (get data :_id)) (dissoc data :_id)))
 
 (defn insert-single [element, collection]
-  (#(mongo-collection/insert collection (merge element {:_id (ObjectId.)}))))
+  (mongo-collection/insert collection (merge element {:_id (ObjectId.)})))
 
 (defn find-popular-new []
-  (#(mongo-collection/aggregate "cases" [{$unwind "$items"}, {$group {:_id "$slug" :sum_items {$sum 1}}}, {$match {:sum_items {$gte 3}}}])))
+  (mongo-collection/aggregate "cases" [{$unwind "$items"}, {$group {:_id "$slug" :sum_items {$sum 1}}}, {$match {:sum_items {$gte 3}}}]))
 
 (defn find-newest-headings [page, per-page]
-  (#(doall (query/with-collection "cases"
+  (doall (query/with-collection "cases"
       (query/find {})
       (query/fields search-result-fields)
       (query/sort (sorted-map "items.meeting.date" -1))
-      (query/paginate :page page :per-page per-page)))))
+      (query/paginate :page page :per-page per-page))))
 
 (defn- search-cases [search-query projection page per-page]
-  (#(doall (query/with-collection "cases"
+  (doall (query/with-collection "cases"
               (query/find search-query)
               (query/fields projection)
-              (query/paginate :page page :per-page per-page)))))
+              (query/paginate :page page :per-page per-page))))
 
 (defn search-by-committee [committee_name page per-page]
   (def query {:items.meeting.committee_name committee_name})
@@ -85,10 +85,10 @@
     (search-cases query projection page per-page)))
 
 (defn search [string]
-  (#(map (fn [result] (get result :obj))(search/results-from (search/search "cases" (search-utils/escape-search-string string))))))
+  (map (fn [result] (get result :obj))(search/results-from (search/search "cases" (search-utils/escape-search-string string)))))
 
 (defn count-cases [search-query]
-  (#(mongo-collection/count "cases" search-query)))
+  (mongo-collection/count "cases" search-query))
   
 (defn count-cases-by-committee [committee_name]
   (count-cases {:items.meeting.committee_name committee_name}))
